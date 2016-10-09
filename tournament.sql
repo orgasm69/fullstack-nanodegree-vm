06aa7377fd27be4d17fb9315fa4c533bc3b175a2
Orgasm69 <cichy.shebang@gmail.com>
2016-10-09 13:36:54 +0200

    CL commit

---
+++ tournament.sql
@@ -0,0 +1,48 @@
+-- Baza
+-- Tabele: Players, Matches, Rounds, PlayerStatistics
+
+-- Players: id, nickname, wins, loses, ratio, standing
+-- Matches: id, roundId, winnerId, loserId, draw
+-- Rounds: id, wildcardPlayerId, playersAmount
+-- PlayerStatistics: playerId, nickname, winner, runnerUp, third
+
+DROP DATABASE IF EXISTS Tournament;
+DROP TABLE Players;
+DROP TABLE Matches;
+DROP TABLE PlayerStatistics;
+DROP TABLE Rounds;
+
+CREATE DATABASE Tournament OWNER Vagrant;
+
+CREATE TABLE Players (
+	id SERIAL PRIMARY KEY,
+	nickname varchar(40) NOT NULL CHECK (length(btrim(nickname)) > 0),
+	wins integer DEFAULT 0,
+	loses integer DEFAULT 0,
+	ratio double precision DEFAULT 0.00,
+	standing integer DEFAULT 0
+	);
+
+CREATE TABLE Matches (
+	id SERIAL PRIMARY KEY,
+	roundId integer,
+	winnerId integer,
+	loserId integer,
+	draw boolean
+	);
+
+CREATE TABLE Rounds (
+	id integer PRIMARY KEY,
+	wildcardPlayerId integer,
+	playersAmount integer
+	);
+
+CREATE TABLE PlayerStatistics (
+	playerId integer,
+	nickname varchar(40),
+	winner integer,
+	runnerUp integer,
+	third integer
+	);
+
+
